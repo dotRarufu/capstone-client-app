b604c9e1fe8df0c4082b7eb9aea1e941493b718d
dotRarufu <81721522+dotRarufu@users.noreply.github.com>
2023-09-13 19:51:07 +0800

    add task color coding

--- src/app/pages/project/pages/tasks/task-card.component.ts
+++ src/app/pages/project/pages/tasks/task-card.component.ts
@@ -7,8 +7,10 @@ import {
   SimpleChanges,
   inject,
 } from '@angular/core';
-import { BehaviorSubject, filter, map, switchMap } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { BehaviorSubject, catchError, filter, map, switchMap, tap } from 'rxjs';
 import { AuthService } from 'src/app/services/auth.service';
+import { ProjectService } from 'src/app/services/project.service';
 import { Task } from 'src/app/types/collection';
 import { isNotNull } from 'src/app/utils/isNotNull';
 
@@ -20,8 +22,17 @@ import { isNotNull } from 'src/app/utils/isNotNull';
     <div
       onclick="taskDetails.showModal()"
       class="cursor-pointer rounded-[3px]  border border-base-content/50 bg-base-100 shadow-md"
+      *ngIf="{
+        adviser: adviser$ | async,
+        adviserProjectRole: adviserProjectRole$ | async
+      } as observables"
     >
-      <div class="bg-secondary p-[16px] text-secondary-content">
+      <div
+        class="p-[16px] text-secondary-content"
+        [ngClass]="observables.adviserProjectRole === 't'
+            ? 'bg-blue-500'
+            : 'bg-secondary'"
+      >
         {{ task.title }}
       </div>
       <div
@@ -29,7 +40,7 @@ import { isNotNull } from 'src/app/utils/isNotNull';
       >
         <span class="text-[12px] text-base-content/70"> BY </span>
 
-        {{ adviserName$ | async }}
+        {{ observables.adviser?.name || 'Unnamed' }}
 
         <img
           class="avatar w-8 rounded-full bg-primary/50"
@@ -42,16 +53,25 @@ import { isNotNull } from 'src/app/utils/isNotNull';
   `,
 })
 export class TaskCardComponent implements OnChanges {
+  projectService = inject(ProjectService);
+  route = inject(ActivatedRoute);
+  authService = inject(AuthService);
+
   @Input() setActiveTask: null | ((t: Task) => void) = null;
   @Input({ required: true }) task!: Task;
 
+  projectId = Number(this.route.parent!.snapshot.url[0].path);
   taskSubject = new BehaviorSubject<Task | null>(null);
-
-  authService = inject(AuthService);
-  adviserName$ = this.taskSubject.pipe(
+  adviser$ = this.taskSubject.pipe(
     filter(isNotNull),
-    switchMap((task) => this.authService.getUserData(this.task.assigner_id)),
-    map((adviser) => adviser.name || 'unnamed')
+    switchMap((task) => this.authService.getUserData(this.task.assigner_id))
+  );
+  adviserProjectRole$ = this.adviser$.pipe(
+    switchMap((u) =>
+      this.projectService.getAdviserProjectRole(this.projectId, u.uid)
+    ),
+    tap(v => console.log("v:", v)),
+    catchError((err) => '')
   );
 
   ngOnChanges(changes: SimpleChanges): void {
